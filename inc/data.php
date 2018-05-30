bdc36f3a38e3ab2bac81e915613b59bd71ba7c33
HenryAlonso <39140462+HenryAlonso@users.noreply.github.com>
2018-05-30 19:10:02 -0400

    Add files via upload

---
+++ inc/data.php
@@ -0,0 +1,205 @@
+<?php
+
+//books 
+
+$catalog = [];
+
+$catalog[101] = [
+
+"title" => "The Weight of Blood",
+"img" => "img/media/half-orc-book_1.png",
+"genre" => "Sci-Fi/Fantasy",
+"format" => "Paperback",
+"year" => "2010",
+"category" => "Books",
+"authors" => [
+    "David Dalglish",
+],
+"publisher" => "Createspace Independent Publishing Platform",
+"isbn" => '1450574483'
+];
+
+$catalog[102] = [
+
+"title" => "The King of the Vile",
+"img" => "img/media/half-orc-book_2.png",
+"genre" => "Sci-Fi/Fantasy",
+"format" => "Paperback",
+"year" => "2015",
+"category" => "Books",
+"authors" => [
+    "David Dalglish",
+],
+"publisher" => "Eschaton Press",
+"isbn" => '1507762453'
+];
+
+$catalog[103] = [
+
+"title" => "The Broken Pieces",
+"img" => "img/media/paladins_1.png",
+"genre" => "Sci-Fi/Fantasy",
+"format" => "Paperback",
+"year" => "2012",
+"category" => "Books",
+"authors" => [
+    "David Dalglish",
+],
+"publisher" => "Eschaton Press",
+"isbn" => '9781479161263'
+];
+
+$catalog[104] = [
+
+"title" => "A Dance of Cloaks",
+"img" => "img/media/dance-of-cloaks_2.png",
+"genre" => "Sci-Fi/Fantasy",
+"format" => "Paperback",
+"year" => "2013",
+"category" => "Books",
+"authors" => [
+    "David Dalglish",
+],
+"publisher" => "Eschaton Press",
+"isbn" => '9780316242394',
+];
+
+//Movies
+$catalog[201] =[
+
+"title" => "The Avengers",
+"img" => "img/media/avengers.png",
+"genre" => "Fantasy/Science Fiction",
+"format" => "DVD",
+"year" => 2012,
+"category" => "Movies",
+"director" => "Joss Whedon",
+"writers" => [
+    "Zak Penn"
+],
+"stars" => [
+    "Robert Downey Jr.",
+    "Chris Evans",
+    "Mark Ruffalo",
+    "Chris Hemsworth",
+    "Scarlett Johansson",
+    "Jeremy Renner",
+    "Tom Hiddleston",
+    "Samuel L. Jackson"
+]
+];
+
+$catalog[202] =[
+
+"title" => "Cast Away",
+"img" => "img/media/cast-away.png",
+"genre" => "Drama survival",
+"format" => "DVD",
+"year" => 2000,
+"category" => "Movies",
+"director" => "Robert Zemeckis",
+"writers" => [
+    "William Broyles, Jr.",
+],
+"stars" => [
+    "Tom Hanks",
+    "Helen Hunt",
+    "Nick Searcy"
+]
+];
+
+$catalog[203] =[
+
+"title" => "Pacific Rim",
+"img" => "img/media/pacific-rim.png",
+"genre" => "Science Fiction/Action",
+"format" => "DVD",
+"year" => 2013,
+"category" => "Movies",
+"director" => "Guillermo del Toro",
+"writers" => [
+    "Travis Beacham",
+    "Guillermo del Toro"
+],
+"stars" => [
+    "Charlie Hunnam",
+    "Idris Elba",
+    "Rinko Kikuchi",
+    "Mana Ashida",
+    "Charlie Day",
+    "Max Martini"
+]
+];
+
+$catalog[204] =[
+
+"title" => "Wild Wild West",
+"img" => "img/media/wild-wild-west.png",
+"genre" => "Action/Comedy",
+"format" => "DVD",
+"year" => 1999,
+"category" => "Movies",
+"director" => "Jon Peters",
+"writers" => [
+    "Brent Maddock",
+    "S. S. Wilson",
+    "Jeffrey Price"
+],
+"stars" => [
+    "Will Smith",
+    "Kevin Kline",
+    "Kenneth Branagh",
+    "Salma Hayek"
+]
+];
+
+//Music
+$catalog[301] = [
+
+"title" => "Nightmare",
+"img" => "img/media/avenged.png",
+"genre" => "Heavy Metal/Hard Rock",
+"format" => "CD",
+"year" => 2010,
+"category" => "Music",
+"artist" => "Avenged Sevenfold"
+];
+
+$catalog[302] = [
+
+"title" => "Awake",
+"img" => "img/media/skillet.png",
+"genre" => "Metal/Alternative Metal",
+"format" => "CD",
+"year" => 2009,
+"category" => "Music",
+"artist" => "Skillet"
+];
+
+$catalog[303] = [
+
+"title" => "Hybrid Theory",
+"img" => "img/media/lk-hybrid-theory.png",
+"genre" => "Nu Metal/Rap Metal",
+"format" => "CD",
+"year" => 2000,
+"category" => "Music",
+"artist" => "Linkin Park"
+];
+
+$catalog[304] = [
+
+"title" => "Meteora",
+"img" => "img/media/lk-meteora.png",
+"genre" => "Nu Metal/Rap Metal",
+"format" => "CD",
+"year" => 2003,
+"category" => "Music",
+"artist" => "Linkin Park"
+];
+?>
+
+
+
+
+
